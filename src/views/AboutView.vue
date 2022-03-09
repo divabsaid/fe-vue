be6190252d51c182b52965e37aad5dba2e9123aa
divabsaid <101082272+divabsaid@users.noreply.github.com>
2022-03-09 15:41:07 +0800

    Initial Commit

---
+++ src/views/AboutView.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="relative bg-white">
+    <div class="max-w-7xl mx-auto px-4 sm:px-6">
+      <div
+        class="
+          flex
+          justify-between
+          items-center
+          border-b-2 border-gray-100
+          py-6
+          md:justify-start md:space-x-10
+        "
+      >
+        <div class="flex justify-start lg:w-0 lg:flex-1">
+          <img
+            class="h-8 w-auto sm:h-10"
+            src="https://tailwindui.com/img/logos/workflow-mark-indigo-600.svg"
+            alt=""
+          />
+        </div>
+        <div class="hidden md:flex md:flex-col text-center">
+          <p class="text-base font-medium">INTEGRATED COMMAND CENTER</p>
+          <p class="text-base font-medium text-gray-500">
+            PERUMDA TIRTA SAIL CRUISE PINK BEACH KOMODO
+          </p>
+        </div>
+        <div class="hidden md:flex items-center justify-end md:flex-1 lg:w-0">
+          <img
+            class="inline-block h-10 w-10 rounded-full ring-2 ring-white"
+            src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
+            alt=""
+          />
+          <div class="hidden md:flex md:flex-col ml-2">
+            <p class="text-base font-medium">Direktur</p>
+            <p class="text-base font-medium text-gray-500">Moch Anji Sumarn..</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
